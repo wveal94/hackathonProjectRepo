7ed8cbf8da893ec3e6ccee4fce351a01ab1cc5b2
William R. Veal III <wveal88@gmail.com>
2016-11-25 15:04:04 -0600

    added some new code

--- testCode.java
+++ testCode.java
@@ -1,5 +1,18 @@
 javaCode{
   public static void main(String[] args){
-    System,out.print("Hello World!");
+    String userMessage = getUserMeassge();
+
+    printUserMessage;
+  }
+
+  public static String getUserMessage(){
+    System.out.print("Say something: ");
+    Scanner input = new Scanner(System.in);
+
+    return input.toString();
+  }
+
+  public static void printUserMessage(String userMessage){
+    System.out.print("You said " + userMessage);
   }
 }
